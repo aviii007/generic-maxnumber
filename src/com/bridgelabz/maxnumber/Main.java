61611f56f8b35f83992a38a0aa9d48e7e163a901
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-21 17:38:07 +0530

    Added the welcome message.

---
+++ src/com/bridgelabz/maxnumber/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.maxnumber;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java generic program to find maximum number");
+    }
+}
