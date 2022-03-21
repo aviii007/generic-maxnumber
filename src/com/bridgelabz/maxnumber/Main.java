c81f3ccf930f4ff7374486c5857f03f8eafe9043
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-22 00:44:52 +0530

    Added to find the maximum of three Doubles.

--- src/com/bridgelabz/maxnumber/Main.java
+++ src/com/bridgelabz/maxnumber/Main.java
@@ -11,10 +11,23 @@ public class Main {
         }
         System.out.println("The Greatest Integer value is:- " +max);
     }
+
+    public static void findMaxDouble(double num1,double num2,double num3){
+        double max=num1;
+        if (max<num2){
+            max=num2;
+        }
+        if (max<num3){
+            max=num3;
+        }
+        System.out.println("The Greatest Double value is:- "+max);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Java Generic Program to find maximum number");
         int num1=100,num2=200,num3=300;
         findMaxInteger(num1,num2,num3);
+        double a=20.5,b=40.5,c=60.5;
+        findMaxDouble(a,b,c);
     }
 }
 
