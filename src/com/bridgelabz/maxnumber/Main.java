6252d32cd2cfef3a29ec7a1ad00f88fb7834bad5
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-22 00:39:18 +0530

    Added to find the maximum of three Integers.

--- src/com/bridgelabz/maxnumber/Main.java
+++ src/com/bridgelabz/maxnumber/Main.java
@@ -1,8 +1,20 @@
 package com.bridgelabz.maxnumber;
 
 public class Main {
-
+    public static void findMaxInteger(int num1,int num2,int num3){
+        int max=num1;
+        if (max<num2){
+            max=num2;
+        }
+        if (max<num3){
+            max=num3;
+        }
+        System.out.println("The Greatest Integer value is:- " +max);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to java generic program to find maximum number");
+        System.out.println("Welcome to Java Generic Program to find maximum number");
+        int num1=100,num2=200,num3=300;
+        findMaxInteger(num1,num2,num3);
     }
 }
+
